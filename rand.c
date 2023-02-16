fcb3c5d9f7d3f4a2608e4c3785f8c52e195dca0f
Toby <78794359+TFB-code@users.noreply.github.com>
2023-02-16 20:03:44 +0000

    Add files via upload

---
+++ rand.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+// nanohop waits for the monotonic clock to update.  Use between calls to
+// a random number generator to reduce the chance of duplicate readings at
+// the cost of performance
+void nanohop() {
+
+    struct timespec time;
+    __pthread_clock_gettime(CLOCK_MONOTONIC,&time);
+
+    long time1 = time.tv_nsec,time2 = time.tv_nsec;
+
+    while (time1==time2) {
+        __pthread_clock_gettime(CLOCK_MONOTONIC, &time);
+        time2=time.tv_nsec;
+    }
+}
+
+// random returns a random number between 0 and 1
+float random() {
+
+    struct timespec time;
+    __pthread_clock_gettime(CLOCK_MONOTONIC,&time);
+
+    char srand[10];
+    char fsrand[10];
+    sprintf( srand, "%ld", time.tv_nsec);
+
+    int len=strlen(srand);
+    if (len>2) {
+        if (srand[len - 2] == '0') {
+            srand[len - 2] = '\0';
+        } else {
+            if (srand[len - 1] == '0') {
+                srand[len - 1] = '\0';
+            }
+        }
+    }
+    strrev(srand);
+
+    if (strlen(srand)>1) {
+        sprintf(fsrand,"0.%s",srand+1);
+    } else {
+        sprintf(fsrand,"0.%s",srand);
+    }
+
+    return atof(fsrand);
+}
+
+// rnd returns a random integer between 0 and num inclusive
+int rnd(int num) {
+    num++;
+    return num*random();
+}
